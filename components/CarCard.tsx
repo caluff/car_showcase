b729a831ecd04d58f038985ceb7dea7fff9eb7ed
Caluff <dcaluff@gmail.com>
2023-06-26 21:24:45 -0400

    creando componente CarCard.tsx, region de la api no disponible

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -1,9 +1,25 @@
-import React from 'react';
-
-const CarCard = () => {
+"use client"
+import {CarProps} from "@/types";
+import customButton from "@/components/CustomButton";
+import {calculateCarRent} from "@/utils";
+interface CarCardProps{
+  car:CarProps;
+}
+const CarCard = ({car}:CarCardProps) => {
+  const {city_mpg,year,make,model,transmission,drive}=car;
+  const carRent = calculateCarRent(city_mpg,year)
   return (
-    <div>
-      CarCard
+    <div className={"flex flex-col p-6 justify-center items-start text-black-100 bg-primary-blue-100 hover:bg-white hover:shadow-md rounded-3xl group"}>
+      <div className={"w-full flex justify-between items-start gap-2"}>
+        <h2 className={"text-[22px] leading-[26px] font-bold capitalize"}>
+          {make} {model}
+        </h2>
+      </div>
+      <p>
+        <span>
+          Car Rent...
+        </span>
+      </p>
     </div>
   );
 };
